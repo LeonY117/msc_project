ed497bf3b04d9c10d5374aeffa4d981e42115561
Leon Yao <leon.yao117@gmail.com>
2023-01-15 17:51:57 +0000

    initialize repo folders

---
+++ configs/config.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Model config in json format"""
+
+CFG = {
+    "data": {
+        "path": "",
+        "image_size": 128,
+        "train_val_split_ratio": 0.95,
+    },
+    "train": {
+        "batch_size": 64,
+        "buffer_size": 1000,
+        "epoches": 20,
+    },
+    "model": {
+
+    }
+}
